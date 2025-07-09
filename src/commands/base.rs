c86eadab276c7bf5e4f7e7847d3b8f7e552a27a5
rafaeljohn9 <rafaeljohb@gmail.com>
2025-07-09 11:14:40 +0300

    refactor: removed commonAddArgs in base

--- src/commands/base.rs
+++ src/commands/base.rs
@@ -1,5 +1,4 @@
 use clap::ValueEnum;
-use std::path::PathBuf;
 
 #[derive(Clone, ValueEnum)]
 pub enum TemplateCategory {
@@ -11,11 +10,4 @@ pub enum TemplateCategory {
 
 pub trait Runnable {
     fn run(&self) -> anyhow::Result<()>;
-}
-
-#[derive(Debug, Clone)]
-pub struct CommonAddArgs {
-    pub dir: Option<PathBuf>,
-    pub force: bool,
-    pub all: bool,
 }
