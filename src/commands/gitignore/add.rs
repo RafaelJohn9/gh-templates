26d5b34dcbcb3690a25b22127adc495fd6684eae
rafaeljohn9 <rafaeljohb@gmail.com>
2025-10-04 16:23:03 +0300

    fix: gitignore template names with the '.gitignore' to be parsed correctly.\n\nInitially we used to allow only the template name without the the ext though some users may pass the '.gitignore' ext. It shouldn't fail but be parsed correctly\n
    Signed-off-by: rafaeljohn9 <rafaeljohb@gmail.com>

--- src/commands/gitignore/add.rs
+++ src/commands/gitignore/add.rs
@@ -209,6 +209,13 @@ fn download_templates(
     } else if output.len() == templates.len() {
         // Save each template to its own file as specified in output
         for (template_name, output_file) in templates.iter().zip(output.iter()) {
+            // check if the template_name has  a .gitignore ext rm it to normalize it
+            let template_name = if template_name.ends_with(".gitignore") {
+                template_name.strip_suffix(".gitignore").unwrap()
+            } else {
+                template_name
+            };
+            
             let template_path = find_template_in_cache(template_name, cache)?;
             let url = format!("{}/{}", GITHUB_RAW_BASE, template_path);
 
